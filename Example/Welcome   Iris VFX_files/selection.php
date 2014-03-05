1d44d8daa2c29721b51f008abbbaf06c75dec236
Kuan Butts <kuanbutts@yahoo.com>
2014-03-05 12:03:05 -0500

    loading website correctly this time

---
+++ Example/Welcome   Iris VFX_files/selection.php
@@ -0,0 +1,83 @@
+
+
+@import "http://fonts.googleapis.com/css?family=Open+Sans:200,300,400,600,700";
+
+
+
+
+
+
+
+
+
+
+
+/* Options set in the admin page */
+
+body { 
+	font-family: Open Sans;
+	color: ;
+}
+.sf-menu li.current-menu-item, .pagenavi ul li.disabled a, .pagenavi ul li a:hover, .iconbox.hexagon:hover > span, .filters li a.active, .widget.widget_flickr .flickr_badge_image:hover, .gotoportfolio:hover, .widget.widget_tag_cloud li a:hover, .post .post-gallery.link:hover, .pricing .item.featured .header, .btn:hover, input[type=submit]:hover, .comment-reply-link:hover { background: #00ccaa; } dl.tabs dd.active a, .clientlist .columns div:hover, .widget.widget_flickr .flickr_badge_image:hover, .pricing .item.featured .header h2 { border-color: #00ccaa; } a:hover, footer a:hover, footer .subfooter a:hover, .iconbox.left:hover > span, #scrolltotop:hover i, #header .searchlink:hover, #header .searchlink.open,.single-portfolio .gotoportfolio:hover i { color: #00ccaa; } ::-webkit-selection{ background-color: #00ccaa; } ::-moz-selection{ background-color: #00ccaa; } ::selection{ background-color: #00ccaa; }
+
+.fresco:hover .overlay {
+		background-color: rgba(0,204,170, 0.85);}
+a { 
+	color: ;
+}
+a:hover { 
+	color: ;
+}
+.post .post-title h2 a{
+	color: #00ccaa;
+}
+.post .post-content h1 {
+	color: #00ccaa;
+}
+.post .post-content h2 {
+	color: #00ccaa;
+}
+.post .post-content h3 {
+	color: #00ccaa;
+}
+.post .post-content h4 {
+	color: #D84B3A;
+}
+.post .post-content h5 {
+	}
+.post .post-content h6 {
+	}
+#header .sf-menu li:hover, .sf-menu li.sfHover,
+#header .sf-menu li ul li {
+	background-color: ;
+}
+#header .sf-menu li ul li:hover {
+	background-color: ;
+}
+#header .logo h1 a{
+	}
+#header ul.sf-menu>li>a {
+	}
+#header ul.sf-menu ul>li>a {
+	}
+#header ul.sf-menu>li>a {
+	}
+#header ul.sf-menu ul>li>a {
+	}
+.sidebar .widget h6{
+	}
+
+/* Footer */
+footer {
+			color: ;
+}
+footer .widget h6 {
+	}
+footer a { 
+	color: ;
+}
+footer a:hover { 
+	color: ;
+}
+footer .subfooter {
+	}
